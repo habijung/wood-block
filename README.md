c0c841bbdee4349dc889c602a738ddb4b5a2e594
habijung <habijung0@gmail.com>
2024-07-22 18:00:04 +0900

    bugfix/build-error-on-Windows
    - Add CMake generate options with C compiler, vcpkg
    - Wrap file path with double quotation marks to string
    - Change submodule link SSH to HTTPS for public link
    - Update README.md

--- README.md
+++ README.md
@@ -4,33 +4,34 @@
 
 ### Environment
 
-| Name   | Version |
-|:-------|:--------|
-| OpenGL | 4.1     |
-| CMake  | 3.29.2  |
-| Ninja  | 1.12.0  |
-| GCC    | 13.2.0  |
+| Name               | Version |
+|:-------------------|:--------|
+| OpenGL             | 4.1     |
+| CMake              | 3.29.2  |
+| Ninja              | 1.12.0  |
+| GCC                | 12.2.0  |
+| Visual Studio 2022 | 17.8.12 |
 
 ### Build
 
-**Use shell script**
-
 ```shell
+# Clone repository
+git clone https://github.com/habijung/untitled.git
+
+# Update submodule
+cd untitled
+git submodule update --init --recursive
+
+# CMake generate and build
 sh build.sh
-```
-
-**Use command line**
-
-```shell
-cmake -S . -B cmake-build -G Ninja -D CMAKE_BUILD_TYPE="Debug"
-cmake --build cmake-build -j 10
 ```
 
 ### Run
 
 ```shell
+# Windows
 ./cmake-build/src/untitled.exe
 ```
 
 ---
-**Last Updated:** 24.07.16
+**Last Updated:** 24.07.22
--- build.sh
+++ build.sh
@@ -1,11 +1,42 @@
 #!/usr/bin/env sh
 
+# Set variables
+SOURCE_DIR=$(dirname $(realpath "$0"))
+C_COMPILER=$(which gcc)
+CXX_COMPILER=$(which g++)
+NINJA=$(which ninja)
+OS=$(uname)
+
+# If OS is Windows, add EXE (.exe) extention
+case $OS in
+MSYS* | MINGW* | CYGWIN*)
+	OS="Windows"
+	C_COMPILER="$C_COMPILER.exe"
+	CXX_COMPILER="$CXX_COMPILER.exe"
+	NINJA="$NINJA.exe"
+	;;
+esac
+
+echo
+echo "------------------------------------------"
+echo "OS          : $OS"
+echo "Source Dir  : $SOURCE_DIR"
+echo "C Compiler  : $C_COMPILER"
+echo "CXX Compiler: $CXX_COMPILER"
+echo "Ninja       : $NINJA"
+echo "------------------------------------------"
+echo
+
 # CMake Generate
 cmake \
-    -S . \
-    -B cmake-build \
-    -G Ninja \
-    -D CMAKE_BUILD_TYPE="Debug"
+	-S . \
+	-B cmake-build \
+	-G Ninja \
+	-D CMAKE_BUILD_TYPE="Debug" \
+	-D CMAKE_C_COMPILER="${C_COMPILER}" \
+	-D CMAKE_CXX_COMPILER="${CXX_COMPILER}" \
+	-D CMAKE_MAKE_PROGRAM="${NINJA}" \
+	-D CMAKE_TOOLCHAIN_FILE="${SOURCE_DIR}/vcpkg/scripts/buildsystems/vcpkg.cmake"
 
 # CMake Build
 cmake --build cmake-build -j 10
